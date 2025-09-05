9c82514ab29a091ea4e029975fccaebb139cb407
Nathan <nvm7252@rit.edu>
2025-09-05 16:42:46 -0400

    Updated Vertex struct to include uv, tangent, and normal

--- Vertex.h
+++ Vertex.h
@@ -10,5 +10,7 @@
 struct Vertex
 {
 	DirectX::XMFLOAT3 Position;	    // The local position of the vertex
-	DirectX::XMFLOAT4 Color;        // The color of the vertex
+	DirectX::XMFLOAT2 UV;			// UV Coordinate
+	DirectX::XMFLOAT3 Normal;		// Vertex's Normal (Lighting)
+	DirectX::XMFLOAT3 Tangent;		// Vertex's Tangent (Normal Mapping)
 };
