09e1a6db1b9942bac8256ac85cbffbce34288904
Nathan <nvm7252@rit.edu>
2025-09-06 12:23:27 -0400

    Updated graphics.h/cpp to support using heaps and descriptors

--- Graphics.h
+++ Graphics.h
@@ -36,6 +36,16 @@ namespace Graphics
 	// Debug Layer
 	inline Microsoft::WRL::ComPtr<ID3D12InfoQueue> InfoQueue;
 
+	// Maximum number of constant buffers, assuming each buffer
+	// is 256 bytes or less. Larger buffers are fine, but will
+	// result in fewer buffers in use at any time
+	const unsigned int maxConstantBuffers = 1000;
+	inline Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> CBVSRVDescriptorHeap;
+	inline Microsoft::WRL::ComPtr<ID3D12Resource> CBUploadHeap;
+	D3D12_GPU_DESCRIPTOR_HANDLE FillNextConstantBufferAndGetGPUDescriptorHandle(
+		void* data,
+		unsigned int dataSizeInBytes);
+
 	// --- FUNCTIONS ---
 
 	// Getters
