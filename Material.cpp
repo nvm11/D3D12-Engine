57b4fe4accc3b98614f009b9ede21bb880b01468
Nathan <nvm7252@rit.edu>
2025-09-14 13:19:16 -0400

    created fields and getters/setters for material class

--- Material.cpp
+++ Material.cpp
@@ -1,1 +1,46 @@
 #include "Material.h"
+
+Microsoft::WRL::ComPtr<ID3D12PipelineState> Material::GetPipelineState()
+{
+    return pipelineState;
+}
+
+DirectX::XMFLOAT2 Material::GetUVScale()
+{
+    return scale;
+}
+
+DirectX::XMFLOAT2 Material::GetUVOffset()
+{
+    return offset;
+}
+
+DirectX::XMFLOAT3 Material::GetColorTint()
+{
+    return colorTint;
+}
+
+D3D12_GPU_DESCRIPTOR_HANDLE Material::GetFinalGPUHandleForSRVs()
+{
+    return finalGPUHandleForSRVs;
+}
+
+void Material::SetPipelineState(Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState)
+{
+    this->pipelineState = pipelineState;
+}
+
+void Material::SetUVScale(DirectX::XMFLOAT2 scale)
+{
+    this->scale = scale;
+}
+
+void Material::SetUVOffset(DirectX::XMFLOAT2 offset)
+{
+    this->offset = offset;
+}
+
+void Material::SetColorTint(DirectX::XMFLOAT3 colorTint)
+{
+    this->colorTint = colorTint;
+}
--- Material.h
+++ Material.h
@@ -1,5 +1,36 @@
 #pragma once
+#include <DirectXMath.h>
+#include <d3d12.h>
+#include <wrl/client.h>
 class Material
 {
+public:
+	// --FUNCTIONS --
+
+	// Getters
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> GetPipelineState();
+	DirectX::XMFLOAT2 GetUVScale();
+	DirectX::XMFLOAT2 GetUVOffset();
+	DirectX::XMFLOAT3 GetColorTint();
+	D3D12_GPU_DESCRIPTOR_HANDLE GetFinalGPUHandleForSRVs();
+
+	// Setters
+	void SetPipelineState(Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState);
+	void SetUVScale(DirectX::XMFLOAT2 scale);
+	void SetUVOffset(DirectX::XMFLOAT2 offset);
+	void SetColorTint(DirectX::XMFLOAT3 colorTint);
+	
+private:
+	// Material properties
+	DirectX::XMFLOAT3 colorTint;
+	DirectX::XMFLOAT2 scale; // uv scale
+	DirectX::XMFLOAT2 offset; // uv offset
+	
+	bool finalized;
+	
+	// Pipeline state and descriptors
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState;
+	D3D12_CPU_DESCRIPTOR_HANDLE textureSRVsBySlot[128]; // 128 textures can be bound per shader stage
+	D3D12_GPU_DESCRIPTOR_HANDLE finalGPUHandleForSRVs;
 };
 
