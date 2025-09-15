01c0eec8d2dfd5f0da79f311fbaa879e49137498
Nathan <nvm7252@rit.edu>
2025-09-14 20:34:54 -0400

    Completed constructor for materials

--- Material.cpp
+++ Material.cpp
@@ -1,46 +1,61 @@
 #include "Material.h"
 
+Material::Material(Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState,
+	DirectX::XMFLOAT3 colorTint,
+	DirectX::XMFLOAT2 scale,
+	DirectX::XMFLOAT2 offset)
+	: pipelineState(pipelineState),
+	colorTint(colorTint),
+	scale(scale),
+	offset(offset),
+	finalized(false),
+	highestSRVIndex(-1)
+{
+	finalGPUHandleForSRVs = {};
+	ZeroMemory(textureSRVsBySlot, sizeof(D3D12_CPU_DESCRIPTOR_HANDLE) * 128);
+}
+
 Microsoft::WRL::ComPtr<ID3D12PipelineState> Material::GetPipelineState()
 {
-    return pipelineState;
+	return pipelineState;
 }
 
 DirectX::XMFLOAT2 Material::GetUVScale()
 {
-    return scale;
+	return scale;
 }
 
 DirectX::XMFLOAT2 Material::GetUVOffset()
 {
-    return offset;
+	return offset;
 }
 
 DirectX::XMFLOAT3 Material::GetColorTint()
 {
-    return colorTint;
+	return colorTint;
 }
 
 D3D12_GPU_DESCRIPTOR_HANDLE Material::GetFinalGPUHandleForSRVs()
 {
-    return finalGPUHandleForSRVs;
+	return finalGPUHandleForSRVs;
 }
 
 void Material::SetPipelineState(Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState)
 {
-    this->pipelineState = pipelineState;
+	this->pipelineState = pipelineState;
 }
 
 void Material::SetUVScale(DirectX::XMFLOAT2 scale)
 {
-    this->scale = scale;
+	this->scale = scale;
 }
 
 void Material::SetUVOffset(DirectX::XMFLOAT2 offset)
 {
-    this->offset = offset;
+	this->offset = offset;
 }
 
 void Material::SetColorTint(DirectX::XMFLOAT3 colorTint)
 {
-    this->colorTint = colorTint;
+	this->colorTint = colorTint;
 }
--- Material.h
+++ Material.h
@@ -5,6 +5,12 @@
 class Material
 {
 public:
+	// --CONSTRUCTORS--
+	Material(Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState,
+		DirectX::XMFLOAT3 colorTint = DirectX::XMFLOAT3(0, 0, 0),
+		DirectX::XMFLOAT2 scale = DirectX::XMFLOAT2(0, 0),
+		DirectX::XMFLOAT2 offset = DirectX::XMFLOAT2(0, 0));
+
 	// --FUNCTIONS --
 
 	// Getters
@@ -19,17 +25,17 @@ public:
 	void SetUVScale(DirectX::XMFLOAT2 scale);
 	void SetUVOffset(DirectX::XMFLOAT2 offset);
 	void SetColorTint(DirectX::XMFLOAT3 colorTint);
-	
+
 private:
 	// Material properties
 	DirectX::XMFLOAT3 colorTint;
-	DirectX::XMFLOAT2 scale; // uv scale
-	DirectX::XMFLOAT2 offset; // uv offset
-	
-	bool finalized;
-	
+	DirectX::XMFLOAT2 scale = DirectX::XMFLOAT2(1, 1); // uv scale
+	DirectX::XMFLOAT2 offset = DirectX::XMFLOAT2(0, 0); // uv offset
+
 	// Pipeline state and descriptors
 	Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState;
+	bool finalized;
+	int highestSRVIndex = 0;
 	D3D12_CPU_DESCRIPTOR_HANDLE textureSRVsBySlot[128]; // 128 textures can be bound per shader stage
 	D3D12_GPU_DESCRIPTOR_HANDLE finalGPUHandleForSRVs;
 };
