9ff38f3a6ccf04ddc7ba6dfbaa616f5a71008bbb
Nathan <nvm7252@rit.edu>
2025-11-05 17:16:03 -0500

    Setup basic pixel shader for particles

---
+++ ParticlesPS.hlsl
@@ -0,0 +1,20 @@
+#include "ShaderStructs.hlsli"
+
+cbuffer externalData : register(b0)
+{
+    float3 colorTint;
+};
+
+
+Texture2D Particle : register(t0);
+SamplerState BasicSampler : register(s0);
+
+// Entry point for this pixel shader
+float4 main(VertexToPixel_Particle input) : SV_TARGET
+{
+	// Sample texture and combine with input color
+    float4 color = Particle.Sample(BasicSampler, input.uv) * input.color;
+    color.rgb *= colorTint;
+    
+    return color;
+}
--- ParticlesVS.hlsl
+++ ParticlesVS.hlsl
@@ -10,5 +10,13 @@ cbuffer externalData : register(b0)
 VertexToPixel_Particle main(VertexShaderInput_Particle input) {
 	// Set up output
     VertexToPixel_Particle output;
+    
+    // Get world position
+    matrix wvp = mul(projection, mul(view, world));
+    output.screenPosition = mul(wvp, float4(input.localPosition, 1.0f));
+
+	// Pass other data through
+    output.uv = input.uv;
+    output.color = input.color;
     return output;
 }
