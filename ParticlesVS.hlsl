fb515e0f68e6c751965c0a56393e3d45c986623a
Nathan <nvm7252@rit.edu>
2025-11-09 12:31:15 -0500

    modified ParticleVS to take in an id param

--- ParticlesVS.hlsl
+++ ParticlesVS.hlsl
@@ -24,7 +24,10 @@ cbuffer externalData : register(b0)
     int constrainYAxis;
 };
 
-VertexToPixel_Particle main(VertexShaderInput_Particle input) {
+// Buffer of particle data
+StructuredBuffer<Particle> ParticleData : register(t0);
+// Take in an ID for the vertex
+VertexToPixel_Particle main(uint id : SV_VertexID) {
 	// Set up output
     VertexToPixel_Particle output;
     
